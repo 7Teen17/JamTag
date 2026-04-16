ba3d898f041badc8633473b85be2be4d6208cafd
7Teen17 <dominic.siino@gmail.com>
2026-04-16 12:30:59 -0700

    chore: move src to src folder

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-import { HapticTab } from "@/components/haptic-tab";
-import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Colors } from "@/constants/theme";
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import { HapticTab } from "@/src/components/haptic-tab";
+import { IconSymbol } from "@/src/components/ui/icon-symbol";
+import { Colors } from "@/src/constants/theme";
+import { useColorScheme } from "@/src/hooks/use-color-scheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,7 +12,8 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[(colorScheme === "dark" ? "dark" : "light")].tint,
+        tabBarActiveTintColor:
+          Colors[colorScheme === "dark" ? "dark" : "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
       }}
