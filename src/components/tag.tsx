6cd0bf57b7b6169825d5adc50508faefc7ac5863
7Teen17 <dominic.siino@gmail.com>
2026-08-25 12:03:35 -0700

    feat: style addable tag

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react-native";
+import { Plus, X } from "lucide-react-native";
 import { StyleSheet, View } from "react-native";
 import { ThemedText } from "./default/themed-text";
 
@@ -6,18 +6,24 @@ type TagProps = {
   value: string;
   type?: "regular" | "large";
   removeable?: boolean;
+  addable?: boolean;
 };
 
-export default function Tag({ value, type, removeable }: TagProps) {
+export default function Tag({ value, type, removeable, addable }: TagProps) {
   const isLarge = type ? type === "large" : false;
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, addable && styles.addableContainer]}>
       <ThemedText type="tag" style={isLarge ? styles.largeText : styles.text}>
         {value}
       </ThemedText>
       {removeable && (
         <View style={{ paddingHorizontal: 5 }}>
           <X color="white" size={14} strokeWidth={3} />
+        </View>
+      )}
+      {addable && (
+        <View style={{ paddingHorizontal: 5 }}>
+          <Plus color="white" size={14} strokeWidth={3} />
         </View>
       )}
     </View>
@@ -31,12 +37,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignSelf: "flex-start",
     flexDirection: "row",
+    alignContent: "center",
+    maxWidth: "100%",
+  },
+  addableContainer: {
+    backgroundColor: "rgba(255, 0, 0, 0.5)",
+    borderWidth: 2,
+    borderStyle: "dashed",
+    borderColor: "white",
   },
   text: {
     paddingHorizontal: 5,
     paddingVertical: 2,
   },
   largeText: {
+    flexShrink: 1,
     fontSize: 20,
     paddingLeft: 10,
     paddingVertical: 4,
